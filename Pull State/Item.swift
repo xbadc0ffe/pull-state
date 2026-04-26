e2d4b6b62b46b179e40581175622b8c5e4b2111d
xbadc0ffe <rkmdevo@gmail.com>
2026-04-26 12:37:12 -0700

    Initial Commit

---
+++ Pull State/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Pull State
+//
+//  Created by Robert Morris on 4/26/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Pull State/Pull_StateApp.swift
@@ -0,0 +1,32 @@
+//
+//  Pull_StateApp.swift
+//  Pull State
+//
+//  Created by Robert Morris on 4/26/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Pull_StateApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
